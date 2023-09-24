4367363e1a330227969ddde356de35dc6915dfa8
Boburjon247 <abdunazarov247@gmail.com>
2023-09-24 21:47:02 +0500

    todo list start js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(){
+   'use strict'
+   // theme window background
+
+   const themeColor = [
+      '#788CDE',
+      '#BC7ABC',
+      '#E46C8C',
+      '#E46B67',
+      '#4AA079',
+      '#479E98',
+      '#8795A0',
+      '#A0CBF1',
+      '#D6BDE7',
+      '#F5B6C2',
+      '#ECBDA2',
+      '#A48163',
+      '#9AD2BA',
+      '#8BD3CE',
+      '#BAC8D4'
+   ]
+   const themePhoto = [
+      '39662.jpg',
+      '39664.jpg',
+      '39666.jpg',
+      '39667.jpg',
+      'f8953b820be0093d552e70a5408622c1.png',
+   ]
+
+})
