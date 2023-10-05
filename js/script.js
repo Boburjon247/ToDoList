823ac20108fc311a525c3a5949276483ebd5c414
Boburjon247 <abdunazarov247@gmail.com>
2023-10-05 18:54:41 +0500

    Add localStorge

--- js/script.js
+++ js/script.js
@@ -10,77 +10,94 @@ document.addEventListener("DOMContentLoaded", () => {
     "39667.png",
     "EDjOfUE.png",
     "f8953b820be0093d552e70a5408622c1.png",
+    "in-the-void-3840x2160-v0-22yvam0r0b5a1.png",
   ];
   for (let key of themePhotoArr) {
-    themePhoto.forEach(element =>{
+    themePhoto.forEach((element) => {
       element.insertAdjacentHTML(
         "afterbegin",
         `<li class = "themeItems" style = "background-image: url(../image/${key})"></li>`
       );
-    })
+    });
   }
 
+  // theme window show
+  const themeColorButton = document.querySelectorAll(".themeColorButton"),
+    ThemeColorWindow = document.querySelectorAll(".show_theme"),
+    themeColorItemsClick = document.querySelectorAll(".themeItems");
 
-  // theme window show 
-  const themeColorButton = document.querySelectorAll('.themeColorButton'),
-    ThemeColorWindow = document.querySelectorAll('.show_theme'),
-    themeColorItemsClick = document.querySelectorAll('.themeItems');
-
-  themeColorButton.forEach((element, i)=>{
-    element.addEventListener('click', ()=>{
+  themeColorButton.forEach((element, i) => {
+    element.addEventListener("click", () => {
       ThemeColorWindow.forEach((item, index) => {
-        if(i == index){
-          item.classList.toggle('active');
-          if(item.classList.contains('active')){
-            element.classList.remove('fa-ellipsis');
-            element.classList.add('fa-xmark');
+        if (i == index) {
+          item.classList.toggle("active");
+          if (item.classList.contains("active")) {
+            element.classList.remove("fa-ellipsis");
+            element.classList.add("fa-xmark");
+          } else {
+            element.classList.remove("fa-xmark");
+            element.classList.add("fa-ellipsis");
           }
-          else{
-            element.classList.remove('fa-xmark');
-            element.classList.add('fa-ellipsis');
-          }
-        }          
+        }
       });
     });
   });
-  themeColorItemsClick.forEach((e,index)=>{
-    e.addEventListener('click',()=>{
-      document.querySelectorAll('.result_wdw').forEach((element,index)=>{
-        if(element.classList.contains('task_container0') && element.classList.contains('active')){
-          localStorageSetImg(e, 0)
+  themeColorItemsClick.forEach((e, index) => {
+    e.addEventListener("click", () => {
+      document.querySelectorAll(".result_wdw").forEach((element, index) => {
+        if (
+          element.classList.contains("task_container0") &&
+          element.classList.contains("active")
+        ) {
+          localStorageSetImg(e, 0);
+        } else if (
+          element.classList.contains("task_container1") &&
+          element.classList.contains("active")
+        ) {
+          localStorageSetImg(e, 1);
+        } else if (
+          element.classList.contains("task_container2") &&
+          element.classList.contains("active")
+        ) {
+          localStorageSetImg(e, 2);
+        } else if (
+          element.classList.contains("task_container3") &&
+          element.classList.contains("active")
+        ) {
+          localStorageSetImg(e, 3);
+        } else if (
+          element.classList.contains("task_container4") &&
+          element.classList.contains("active")
+        ) {
+          localStorageSetImg(e, 4);
+        } else if (
+          element.classList.contains("task_container5") &&
+          element.classList.contains("active")
+        ) {
+          localStorageSetImg(e, 5);
+        } else if (
+          element.classList.contains("task_container6") &&
+          element.classList.contains("active")
+        ) {
+          localStorageSetImg(e, 6);
         }
-        else if(element.classList.contains('task_container1') && element.classList.contains('active')){
-          localStorageSetImg(e, 1)
-        }
-        else if(element.classList.contains('task_container2') && element.classList.contains('active')){
-          localStorageSetImg(e, 2)
-        }
-        else if(element.classList.contains('task_container3') && element.classList.contains('active')){
-          localStorageSetImg(e, 3)
-        }
-        else if(element.classList.contains('task_container4') && element.classList.contains('active')){
-          localStorageSetImg(e, 4)
-        }
-        else if(element.classList.contains('task_container5') && element.classList.contains('active')){
-          localStorageSetImg(e, 5)
-        }
-        else if(element.classList.contains('task_container6') && element.classList.contains('active')){
-          localStorageSetImg(e, 6)
-        }
-
       });
     });
   });
-  function localStorageSetImg(e, name){
-    let bg  = e.style.backgroundImage
+  function localStorageSetImg(e, name) {
+    let bg = e.style.backgroundImage;
     localStorage.setItem(`bg${name}`, bg);
     const bgGet = localStorage.getItem(`bg${name}`);
-    document.querySelector(`.task_container${name} .task_container`).style.backgroundImage = bgGet;  
+    document.querySelector(
+      `.task_container${name} .task_container`
+    ).style.backgroundImage = bgGet;
   }
   for (let index = 0; index < 7; index++) {
-    let bgGet = localStorage.getItem(`bg${index}`)
-    document.querySelector(`.task_container${index} .task_container`).style.backgroundImage = bgGet;  
-  }  
+    let bgGet = localStorage.getItem(`bg${index}`);
+    document.querySelector(
+      `.task_container${index} .task_container`
+    ).style.backgroundImage = bgGet;
+  }
   // accounts
   const userName = document.querySelector(".user_name");
   const showAccount = document.querySelector(".showAcout");
@@ -130,8 +147,8 @@ document.addEventListener("DOMContentLoaded", () => {
     );
   }
   const li = document.querySelectorAll(".menuItem li");
-  const result_window = document.querySelectorAll('.result_wdw'); 
-  // control_window click  
+  const result_window = document.querySelectorAll(".result_wdw");
+  // control_window click
   function onClick(activeIndex) {
     li.forEach((element, index) => {
       element.classList.remove("active");
@@ -139,13 +156,15 @@ document.addEventListener("DOMContentLoaded", () => {
         element.classList.add("active");
       }
     });
-    result_window.forEach((element, index)=>{
-      element.classList.remove('active');
-      if(activeIndex == index){
-        element.classList.add('active');
+    result_window.forEach((element, index) => {
+      element.classList.remove("active");
+      if (activeIndex == index) {
+        element.classList.add("active");
       }
     });
-
+    inputAddElement.forEach((element) => {
+      element.value = "";
+    });
   }
   li.forEach((element, index) => {
     element.addEventListener("click", () => {
@@ -153,33 +172,31 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   // add_task input
-  const addTaskInput = document.querySelectorAll('.add_task input');
-  const addTaskIcon = document.querySelectorAll('.add_task i');
-  window.addEventListener('click', (e)=>{
-    if(e.target.classList == 'input'){
-      addTaskInput.forEach(element => {
+  const addTaskInput = document.querySelectorAll(".add_task .input");
+  const addTaskIcon = document.querySelectorAll(".add_task i");
+  window.addEventListener("click", (e) => {
+    if (e.target.classList == "input") {
+      addTaskInput.forEach((element) => {
         element.placeholder = `Try typing 'Pay utilities bill by Friday 6pm`;
-        element.removeAttribute('readonly');
+        element.removeAttribute("readonly");
       });
-      addTaskIcon.forEach(element =>{
-        element.classList.remove('fa-plus');
-        element.classList.add('fa-circle');
-      })
-    }
-    else{
-      addTaskInput.forEach(element =>{
+      addTaskIcon.forEach((element) => {
+        element.classList.remove("fa-plus");
+        element.classList.add("fa-circle");
+      });
+    } else {
+      addTaskInput.forEach((element) => {
         element.placeholder = `Add a text`;
-        element.setAttribute('readonly', '');
-      })
-      addTaskIcon.forEach(element => {
-        element.classList.remove('fa-circle');
-        element.classList.add('fa-plus')
-      })      
+        element.setAttribute("readonly", "");
+      });
+      addTaskIcon.forEach((element) => {
+        element.classList.remove("fa-circle");
+        element.classList.add("fa-plus");
+      });
     }
   });
-
   // my day date
-  const nowDay = document.querySelector('.nowDay span')
+  const nowDay = document.querySelector(".nowDay span");
   const date = new Date();
   const month = date.getMonth();
   const monthNumber = date.getDate();
@@ -187,30 +204,174 @@ document.addEventListener("DOMContentLoaded", () => {
   let weekString;
   let monthString;
   switch (month) {
-    case 0:monthString = 'January';break;
-    case 1:monthString = 'February';break;
-    case 2:monthString = 'March';break;
-    case 3:monthString = 'April';break;
-    case 4:monthString = 'May';break;
-    case 5:monthString = 'June';break;
-    case 6:monthString = 'July';break;
-    case 7:monthString = 'August';break;
-    case 8:monthString = 'September';break;
-    case 9:monthString = 'October';break;
-    case 10:monthString = 'November';break;
-    case 11:monthString = 'December';break;
+    case 0:
+      monthString = "January";
+      break;
+    case 1:
+      monthString = "February";
+      break;
+    case 2:
+      monthString = "March";
+      break;
+    case 3:
+      monthString = "April";
+      break;
+    case 4:
+      monthString = "May";
+      break;
+    case 5:
+      monthString = "June";
+      break;
+    case 6:
+      monthString = "July";
+      break;
+    case 7:
+      monthString = "August";
+      break;
+    case 8:
+      monthString = "September";
+      break;
+    case 9:
+      monthString = "October";
+      break;
+    case 10:
+      monthString = "November";
+      break;
+    case 11:
+      monthString = "December";
+      break;
   }
   switch (weekNumber) {
-    case 0:weekString = 'Su';break;
-    case 1:weekString = 'Mo';break;
-    case 2:weekString = 'Tu';break;
-    case 3:weekString = 'We';break;
-    case 4:weekString = 'Th';break;
-    case 5:weekString = 'Fr';break;
-    case 6:weekString = 'Sa';break;
+    case 0:
+      weekString = "Sunday";
+      break;
+    case 1:
+      weekString = "Monday";
+      break;
+    case 2:
+      weekString = "Tuesday";
+      break;
+    case 3:
+      weekString = "Wednesday";
+      break;
+    case 4:
+      weekString = "Thursday";
+      break;
+    case 5:
+      weekString = "Friday";
+      break;
+    case 6:
+      weekString = "Saturday";
+      break;
   }
   nowDay.innerHTML = `${weekString}, ${monthNumber} ${monthString}`;
 
+  const mainContainer = document.querySelectorAll(".mainContainer");
+  const task_modal = document.querySelectorAll(".task_modal");
+  function checkActive(item1, item2, item3) {
+    item1.forEach((element) => {
+      if (element.classList.contains("active")) {
+        item2.forEach((element, index) => {
+          if (element.childElementCount >= 1) {
+            item3.forEach((e, i) => {
+              if (index == i) {
+                e.classList.add("active");
+              }
+            });
+          } else {
+            item3.forEach((e) => {
+              if (index == i) {
+                e.classList.remove("active");
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+  checkActive(
+    document.querySelectorAll(".result_wdw"),
+    mainContainer,
+    task_modal
+  );
 
+  // class Contact{
+  //   constructor(change, text, important){
+  //     this.change = change
+  //     this.text = text
+  //     this.important = important
+  //   }
+  //   get changeOnclick(){return this.change}
+  //   get textOnclick(){return this.text}
+  //   get importantOnclick(){return this.important}
+  // }
+  const inputAddElement = document.querySelectorAll("#inputAdd");
+  const WindowActive = document.querySelectorAll(".result_wdw");
+  let mainContactListArray = JSON.parse(localStorage.getItem("array")) ?? [];
+  let contactListObject = {
+    id: null,
+    text: "",
+    important: false,
+    change: false,
+  };
+  inputAddElement.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      WindowActive.forEach((e, i) => {
+        if (index == i) {
+          document.addEventListener("keydown", (e) => {
+            if (e.code == "Enter" && element.value != "") {
+              contactListObject = {
+                id: index,
+                text: element.value,
+                important: false,
+                change: false,
+              };
 
+              let JsonString;
+              let StringJson;
+              JsonString = JSON.stringify(contactListObject);
+              mainContactListArray.push(JsonString);
+              StringJson = JSON.stringify(mainContactListArray);
+              localStorage.setItem("array", StringJson);
+            }
+          });
+        }
+      });
+    });
+  });
+
+  const array = [
+    {
+      id: 0,
+      change: true,
+      text: "Bobur",
+      important: true,
+    },
+    {
+      id: 1,
+      change: true,
+      text: "Bobur",
+      important: true,
+    },
+    {
+      id: 2,
+      change: true,
+      text: "Bobur",
+      important: true,
+    },
+    {
+      id: 3,
+      change: true,
+      text: "Bobur",
+      important: true,
+    },
+  ];
+
+  // document.addEventListener('keydown', (event)=>{
+  //   if(event.code == 'Enter'){
+  //     inputAddElement.forEach(element=>{
+
+  //     })
+  //   }
+  // });
 });
